71e52c728976624fd8f2d920274c5b0dc56a4db4
Strobelt Hendrik <hendrik@strobelt.com>
2020-08-03 21:05:16 -0400

    .

--- client/src/ts/etc/Util.ts
+++ client/src/ts/etc/Util.ts
@@ -80,3 +80,9 @@ export function token_cleanup(token) {
     }
     return token;
 }
+
+// Kudos: https://stackoverflow.com/questions/9401312/how-to-replace-curly-quotation-marks-in-a-string-using-javascript#answer-9401374
+export const cleanSpecials = input => input
+    .replace(/[\u2018\u2019]/g, "'")
+    .replace(/[\u201C\u201D]/g, '"')
+    .replace(/[\u2013\u2014]/g, "-");
